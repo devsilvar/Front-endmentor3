e5cb905076b2448bc3cf27e3be757ef0f1f5a891
Aiyegbajeje Yusuf <refsilva@yahoo.com>
2022-08-25 04:34:37 +0100

    Update fileJs

--- file.js
+++ file.js
@@ -1,77 +1,55 @@
+let fromRep = document.querySelector("form");
+let cardRep = document.querySelector(".container");
+let btnRep = document.querySelectorAll(".circle");
+let submitRep = document.querySelector(".submit");
+
+let selected;
+
+btnRep.forEach((btn) => {
+    btn.addEventListener('click' , (normalEvevnt) => {
+        normalEvevnt.preventDefault();
+        selected = btn.value;
+          changer();
+    })
+})
 
 
-
-
-document.querySelector(".A").onclick = function(){
-    const circles = document.querySelector(".A");
-    circles.style.backgroundColor="hsl(25, 97%, 53%)"; 
-    circles.style.color="white";
-    document.querySelector('.B').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.C').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.D').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.E').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.results').innerHTML = "You selected 1 out of 5";
-
-}
-
-
-document.querySelector(".B").onclick = function(){
-    const circles = document.querySelector(".B");
-    circles.style.backgroundColor="hsl(25, 97%, 53%)"; 
-    circles.style.color="white";
-    document.querySelector('.A').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.C').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.D').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.E').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.results').innerHTML = "You selected 2 out of 5";
-}
-
-document.querySelector(".C").onclick = function(){
-    const circles = document.querySelector(".C");
-    circles.style.backgroundColor="hsl(25, 97%, 53%)";
-    circles.style.color="white"; 
-    document.querySelector('.A').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.B').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.D').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.E').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.results').innerHTML = "You selected 3 out of 5";
-}
-
-document.querySelector(".D").onclick = function(){
-    const circles = document.querySelector(".D");
-    circles.style.backgroundColor="hsl(25, 97%, 53%)";
-    circles.style.color="white"; 
-    document.querySelector('.A').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.C').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.B').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.E').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.results').innerHTML = "You selected 4 out of 5";
-}
-document.querySelector(".E").onclick = function(){
-    const circles = document.querySelector(".E");
-    circles.style.backgroundColor="hsl(25, 97%, 53%)"; 
-    circles.style.color="white"; 
-    document.querySelector('.A').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.C').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.D').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.B').style.backgroundColor = "hsl(216, 12%, 23%)";
-    document.querySelector('.results').innerHTML = "You selected 5 out of 5";
-}
-
-document.querySelectorAll(".circles").mouseover = function(){
-    document.querySelector(".circles").style.backgroundColor = "hsl(217, 12%, 63%)"
-};
-
-
-
-document.querySelector("#btn").onclick = function(){
-    document.querySelector("#btn").style.backgroundColor = "white";
-    document.querySelector("#btn").style.color = "orange";
-    document.querySelector("#btn").style.fontWeight = 900;
-
-    setTimeout ((function(){  document.querySelector(".thanks-card").style.display = "block";
-    document.querySelector(".card").style.display = "none";}), 200)
-
+function changer(){
+  btnRep.forEach((btn) => {
+      //remove all clasese defeault to chnage one of the sleleted coclor to orange
+      btn.classList.remove("is-selected");
     
+ 
+      if(btn.getAttribute("value") === selected){
+          //add the class back
+          btn.classList.add("is-selected") ;
+     }
+  })
+}
 
-};
+
+fromRep.addEventListener('submit' , (res) => {
+   res.preventDefault();
+
+   document.querySelector("#btn").style.backgroundColor = "white";
+   document.querySelector("#btn").style.color = "orange";
+   document.querySelector("#btn").style.fontWeight = 900;
+
+   setTimeout ((function(){  
+
+    if(selected != undefined){ 
+
+      cardRep.innerHTML = `<div class="Thanks">
+        <img src="images/illustration-thank-you.svg" id="thanks-icon" alt="">
+  
+          <p class="results">You selected ${selected} out of 5</p>
+  
+          <h1>Thank You!</h1>
+          <p>We appreciate you taking the time to give a rating if you ever need more support don't hesistate to get in touch</p>
+        </div>`;
+   }
+    }), 1000)
+
+
+
+})
